8048610b9ce5f2bdfde03915c7571665554fdfd2
AB Chan <chanabnus@gmail.com>
2022-12-08 22:57:26 +0800

    init commit

---
+++ src/Secured.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import Keycloak from 'keycloak-js';
+
+class Secured extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { keycloak: null, authenticated: false };
+  }
+
+  componentDidMount() {
+    // const keycloak = Keycloak('/keycloak.json');
+    const keycloak = new Keycloak({
+        "realm": "keycloak-auth",
+        "clientId": "keycloak-auth-client",
+        "auth-server-url": "http://localhost:8080/auth",
+        "ssl-required": "external",
+        "resource": "keycloak-auth-client",
+        "public-client": true,
+        "confidential-port": 0,
+        "onLoad": "login-required"
+        // "onLoad": 'check-sso'
+      });
+    keycloak.init({onLoad: 'login-required'}).then(authenticated => {
+      this.setState({ keycloak: keycloak, authenticated: authenticated })
+    })
+  }
+
+  render() {
+    if (this.state.keycloak) {
+      if (this.state.authenticated) return (
+        <div>
+          <p>This is a Keycloak-secured component of your application. You shouldn't be able
+          to see this unless you've authenticated with Keycloak.</p>
+        </div>
+      ); else return (<div>Unable to authenticate!</div>)
+    }
+    return (
+      <div>Initializing Keycloak...</div>
+    );
+  }
+}
+export default Secured;
